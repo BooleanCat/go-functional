4d8bf78036db915079aaa713c2627941ceb524de
Tom Godkin <thgodkin@gmail.com>
2018-06-26 00:30:55 +0100

    add option generator

--- gen/option.go
+++ gen/option.go
@@ -1,1 +1,31 @@
 package gen
+
+import "github.com/dave/jennifer/jen"
+
+func Option(t string) *jen.File {
+	f := jen.NewFile("f" + t)
+
+	f.Type().Id("Option").Struct(
+		jen.Id("Value").Id(t),
+		jen.Id("present").Bool(),
+	)
+
+	f.Func().Id("Some").Params(jen.Id("value").Id(t)).Id("Option").Block(
+		jen.Return(jen.Id("Option").Values(jen.Dict{
+			jen.Id("Value"):   jen.Id("value"),
+			jen.Id("present"): jen.True(),
+		})),
+	)
+	f.Line()
+
+	f.Func().Id("None").Params().Id("Option").Block(
+		jen.Return(jen.Id("Option").Values()),
+	)
+	f.Line()
+
+	f.Func().Params(jen.Id("o").Id("Option")).Id("Present").Params().Bool().Block(
+		jen.Return(jen.Id("o").Dot("present")),
+	)
+
+	return f
+}
--- gen/option_test.go
+++ gen/option_test.go
@@ -1,12 +1,71 @@
 package gen_test
 
 import (
+	"fmt"
+
+	"github.com/BooleanCat/go-functional/gen"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
 
-var _ = Describe("Gen", func() {
-	It("does nothing", func() {
-		Expect(true).To(BeTrue())
+var _ = Describe("Option", func() {
+	When("string", func() {
+		It("generates expected code", func() {
+			expected := clean(`
+				package fstring
+
+				type Option struct {
+					Value   string
+					present bool
+				}
+
+				func Some(value string) Option {
+					return Option{
+						Value:   value,
+						present: true,
+					}
+				}
+
+				func None() Option {
+					return Option{}
+				}
+
+				func (o Option) Present() bool {
+					return o.present
+				}
+			`)
+
+			Expect(fmt.Sprintf("%#v", gen.Option("string"))).To(Equal(expected))
+		})
+	})
+
+	When("int", func() {
+		It("generates expected code", func() {
+			expected := clean(`
+				package fint
+
+				type Option struct {
+					Value   int
+					present bool
+				}
+
+				func Some(value int) Option {
+					return Option{
+						Value:   value,
+						present: true,
+					}
+				}
+
+				func None() Option {
+					return Option{}
+				}
+
+				func (o Option) Present() bool {
+					return o.present
+				}
+			`)
+
+			Expect(fmt.Sprintf("%#v", gen.Option("int"))).To(Equal(expected))
+		})
 	})
 })
