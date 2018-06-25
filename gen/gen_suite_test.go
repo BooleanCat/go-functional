323a59eac085b76cbf6e4c60f164d8cd7ee1c4d4
Tom Godkin <thgodkin@gmail.com>
2018-06-26 00:30:55 +0100

    add option generator

--- gen/gen_suite_test.go
+++ gen/gen_suite_test.go
@@ -1,13 +1,19 @@
 package gen_test
 
 import (
+	"strings"
 	"testing"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	"github.com/renstrom/dedent"
 )
 
 func TestGen(t *testing.T) {
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "Gen Suite")
+}
+
+func clean(text string) string {
+	return strings.TrimSpace(dedent.Dedent(text)) + "\n"
 }
