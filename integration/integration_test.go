a8ebc7c91a8e3568dfef667bfd8b89a61d7cb246
Tom Godkin <thgodkin@gmail.com>
2018-06-27 22:06:05 +0100

    add integration tests for drop, take, option and functor

--- integration/integration_test.go
+++ integration/integration_test.go
@@ -1,22 +1,235 @@
 package integration_test
 
 import (
-	"os/exec"
+	"os"
+	"path/filepath"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
 
 var _ = Describe("go-functional", func() {
+	var (
+		workDir     string
+		someBinPath string
+	)
+
+	BeforeEach(func() {
+		workDir = tempDir()
+		mkdirAt(workDir, "src", "somebin")
+		someBinPath = filepath.Join(workDir, "src", "somebin")
+	})
+
+	AfterEach(func() {
+		Expect(os.RemoveAll(workDir)).To(Succeed())
+	})
+
 	It("succeeds", func() {
-		cmd := exec.Command(goFunctionalBin, "string")
+		cmd := goFunctionalCommand(someBinPath, "string")
 		Expect(cmd.Run()).To(Succeed())
 	})
 
 	When("the type name is omitted", func() {
 		It("fails", func() {
-			cmd := exec.Command(goFunctionalBin)
+			cmd := goFunctionalCommand(someBinPath)
 			Expect(cmd.Run()).NotTo(Succeed())
+		})
+	})
+
+	It("creates a new package in the working directory", func() {
+		cmd := goFunctionalCommand(someBinPath, "string")
+		Expect(cmd.Run()).To(Succeed())
+
+		cmd = makeFunctionalSample(workDir, "somebin", clean(`
+			package main
+
+			import "somebin/fstring"
+
+			func main() {
+				_ = fstring.Some("foo")
+			}
+		`))
+
+		Expect(cmd.Run()).To(Succeed())
+	})
+
+	Describe("Option", func() {
+		It(`implements option := fint.Some(42)`, func() {
+			cmd := goFunctionalCommand(someBinPath, "int")
+			Expect(cmd.Run()).To(Succeed())
+
+			cmd = makeFunctionalSample(workDir, "somebin", clean(`
+				package main
+
+				import "somebin/fint"
+
+				func main() {
+					option := fint.Some(42)
+
+					if !option.Present() {
+						panic("expected option to hold a value")
+					}
+
+					if option.Value != 42 {
+						panic("expected option to hold 42")
+					}
+				}
+			`))
+
+			Expect(cmd.Run()).To(Succeed())
+		})
+
+		It(`implements option := fint.None()`, func() {
+			cmd := goFunctionalCommand(someBinPath, "int")
+			Expect(cmd.Run()).To(Succeed())
+
+			cmd = makeFunctionalSample(workDir, "somebin", clean(`
+				package main
+
+				import "somebin/fint"
+
+				func main() {
+					option := fint.None()
+
+					if option.Present() {
+						panic("expected option not to hold a value")
+					}
+				}
+			`))
+
+			Expect(cmd.Run()).To(Succeed())
+		})
+	})
+
+	Describe("Functor", func() {
+		It("implements fstring.Lift(slice)", func() {
+			cmd := goFunctionalCommand(someBinPath, "string")
+			Expect(cmd.Run()).To(Succeed())
+
+			cmd = makeFunctionalSample(workDir, "somebin", clean(`
+				package main
+
+				import "somebin/fstring"
+
+				func main() {
+					slice := []string{"foo", "bar"}
+					_ = fstring.Lift(slice)
+				}
+			`))
+
+			Expect(cmd.Run()).To(Succeed())
+		})
+
+		It("implements fstring.Lift(slice).Collect()", func() {
+			cmd := goFunctionalCommand(someBinPath, "string")
+			Expect(cmd.Run()).To(Succeed())
+
+			cmd = makeFunctionalSample(workDir, "somebin", clean(`
+				package main
+
+				import (
+					"fmt"
+					"reflect"
+					"somebin/fstring"
+				)
+
+				func main() {
+					slice := []string{"foo", "bar"}
+					functor := fstring.Lift(slice)
+
+					newSlice := functor.Collect()
+					if !reflect.DeepEqual(newSlice, slice) {
+						panic(fmt.Sprintf("expected %#v to equal %#v", slice, newSlice))
+					}
+				}
+			`))
+
+			Expect(cmd.Run()).To(Succeed())
+		})
+
+		It("implements functor.Fold(a, f)", func() {
+			cmd := goFunctionalCommand(someBinPath, "int")
+			Expect(cmd.Run()).To(Succeed())
+
+			cmd = makeFunctionalSample(workDir, "somebin", clean(`
+				package main
+
+				import (
+					"fmt"
+					"somebin/fint"
+				)
+
+				func main() {
+					functor := fint.Lift([]int{1, 0, 14, 2})
+
+					sum := func(a, b int) int { return a + b }
+
+					result := functor.Fold(0, sum)
+					if result != 17 {
+						panic(fmt.Sprintf("expected %d to equal 17", result))
+					}
+				}
+			`))
+
+			Expect(cmd.Run()).To(Succeed())
+		})
+	})
+
+	Describe("Drop", func() {
+		It("implements functor.Drop(n)", func() {
+			cmd := goFunctionalCommand(someBinPath, "string")
+			Expect(cmd.Run()).To(Succeed())
+
+			cmd = makeFunctionalSample(workDir, "somebin", clean(`
+				package main
+
+				import (
+					"fmt"
+					"reflect"
+					"somebin/fstring"
+				)
+
+				func main() {
+					slice := []string{"foo", "bar", "baz"}
+					newSlice := fstring.Lift(slice).Drop(2).Collect()
+					expected := []string{"baz"}
+
+					if !reflect.DeepEqual(newSlice, expected) {
+						panic(fmt.Sprintf("expected %#v to equal %#v", expected, newSlice))
+					}
+				}
+			`))
+
+			Expect(cmd.Run()).To(Succeed())
+		})
+	})
+
+	Describe("Take", func() {
+		It("implements functor.Take(n)", func() {
+			cmd := goFunctionalCommand(someBinPath, "string")
+			Expect(cmd.Run()).To(Succeed())
+
+			cmd = makeFunctionalSample(workDir, "somebin", clean(`
+				package main
+
+				import (
+					"fmt"
+					"reflect"
+					"somebin/fstring"
+				)
+
+				func main() {
+					slice := []string{"foo", "bar", "baz"}
+					newSlice := fstring.Lift(slice).Take(2).Collect()
+					expected := []string{"foo", "bar"}
+
+					if !reflect.DeepEqual(newSlice, expected) {
+						panic(fmt.Sprintf("expected %#v to equal %#v", expected, newSlice))
+					}
+				}
+			`))
+
+			Expect(cmd.Run()).To(Succeed())
 		})
 	})
 })
--- main.go
+++ main.go
@@ -1,10 +1,43 @@
 package main
 
-import "os"
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+
+	"github.com/BooleanCat/go-functional/gen"
+	"github.com/dave/jennifer/jen"
+)
 
 func main() {
-	_, err := parseArgs()
+	args, err := parseArgs()
 	if err != nil {
 		os.Exit(1)
+	}
+
+	err = os.Mkdir("f"+args.Positional.TypeName, 0755)
+	if err != nil {
+		os.Exit(1)
+	}
+
+	files := map[string]func(string) *jen.File{
+		"drop.go":    gen.Drop,
+		"exclude.go": gen.Exclude,
+		"filter.go":  gen.Filter,
+		"functor.go": gen.Functor,
+		"iter.go":    gen.Iter,
+		"map.go":     gen.Map,
+		"option.go":  gen.Option,
+		"take.go":    gen.Take,
+	}
+
+	for f, generate := range files {
+		path := filepath.Join("f"+args.Positional.TypeName, f)
+		content := fmt.Sprintf("%#v", generate(args.Positional.TypeName))
+		err = ioutil.WriteFile(path, []byte(content), 0755)
+		if err != nil {
+			os.Exit(1)
+		}
 	}
 }
