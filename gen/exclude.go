2ca68091b633a6ef00a2dc32a1b3ee1bcb87540d
Tom Godkin <thgodkin@gmail.com>
2018-06-28 13:19:47 +0100

    add tests for exclude, filter and map

--- gen/exclude.go
+++ gen/exclude.go
@@ -24,7 +24,7 @@ func Exclude(t string) *jen.File {
 
 	f.Func().Params(jen.Id("iter").Id("ExcludeIter")).Id("Next").Params().Id("Option").Block(
 		jen.For().Block(
-			jen.If(jen.Id("option").Op(":=").Id("iter").Dot("iter").Dot("Next").Call().Op(";").Op("!").Id("option").Dot("Present").Call().Op("||").Id("iter").Dot("exclude").Call(jen.Id("option").Dot("Value"))).Block(
+			jen.If(jen.Id("option").Op(":=").Id("iter").Dot("iter").Dot("Next").Call().Op(";").Op("!").Id("option").Dot("Present").Call().Op("||").Op("!").Id("iter").Dot("exclude").Call(jen.Id("option").Dot("Value"))).Block(
 				jen.Return(jen.Id("option")),
 			),
 		),
--- integration/integration_test.go
+++ integration/integration_test.go
@@ -232,4 +232,142 @@ var _ = Describe("go-functional", func() {
 			Expect(cmd.Run()).To(Succeed())
 		})
 	})
+
+	Describe("Exclude", func() {
+		It("implements functor.Exclude(f)", func() {
+			cmd := goFunctionalCommand(someBinPath, "bool")
+			Expect(cmd.Run()).To(Succeed())
+
+			cmd = makeFunctionalSample(workDir, "somebin", clean(`
+				package main
+
+				import (
+					"fmt"
+					"reflect"
+					"somebin/fbool"
+				)
+
+				func main() {
+					slice := []bool{true, false, true}
+					isFalse := func(value bool) bool { return !value }
+					newSlice := fbool.Lift(slice).Exclude(isFalse).Collect()
+					expected := []bool{true, true}
+
+					if !reflect.DeepEqual(newSlice, expected) {
+						panic(fmt.Sprintf("expected %#v to equal %#v", expected, newSlice))
+					}
+				}
+			`))
+
+			Expect(cmd.Run()).To(Succeed())
+		})
+	})
+
+	Describe("Filter", func() {
+		It("implements functor.Filter(f)", func() {
+			cmd := goFunctionalCommand(someBinPath, "bool")
+			Expect(cmd.Run()).To(Succeed())
+
+			cmd = makeFunctionalSample(workDir, "somebin", clean(`
+				package main
+
+				import (
+					"fmt"
+					"reflect"
+					"somebin/fbool"
+				)
+
+				func main() {
+					slice := []bool{true, false, true}
+					isFalse := func(value bool) bool { return !value }
+					newSlice := fbool.Lift(slice).Filter(isFalse).Collect()
+					expected := []bool{false}
+
+					if !reflect.DeepEqual(newSlice, expected) {
+						panic(fmt.Sprintf("expected %#v to equal %#v", expected, newSlice))
+					}
+				}
+			`))
+
+			Expect(cmd.Run()).To(Succeed())
+		})
+	})
+
+	Describe("Map", func() {
+		It("implements functor.Map(f)", func() {
+			cmd := goFunctionalCommand(someBinPath, "int")
+			Expect(cmd.Run()).To(Succeed())
+
+			cmd = makeFunctionalSample(workDir, "somebin", clean(`
+				package main
+
+				import (
+					"fmt"
+					"reflect"
+					"somebin/fint"
+				)
+
+				func main() {
+					slice := []int{0, 1, 2, -1}
+					double := func(value int) int { return value * 2 }
+					newSlice := fint.Lift(slice).Map(double).Collect()
+					expected := []int{0, 2, 4, -2}
+
+					if !reflect.DeepEqual(newSlice, expected) {
+						panic(fmt.Sprintf("expected %#v to equal %#v", expected, newSlice))
+					}
+				}
+			`))
+
+			Expect(cmd.Run()).To(Succeed())
+		})
+	})
+
+	Describe("first 10 prime numbers", func() {
+		It("can be calculated using go-functional", func() {
+			cmd := goFunctionalCommand(someBinPath, "int")
+			Expect(cmd.Run()).To(Succeed())
+
+			cmd = makeFunctionalSample(workDir, "somebin", clean(`
+				package main
+
+				import (
+					"fmt"
+					"reflect"
+					"math"
+					"somebin/fint"
+				)
+
+				type counter struct {
+					i int
+				}
+
+				func (c *counter) Next() fint.Option {
+					option := fint.Some(c.i)
+					c.i++
+					return option
+				}
+
+				func isPrime(value int) bool {
+					for i := 2; i <= int(math.Floor(float64(value) / 2)); i++ {
+						if value%i == 0 {
+							return false
+						}
+					}
+					return value > 1
+				}
+
+				func main() {
+					primes := fint.New(new(counter)).Filter(isPrime).Take(10).Collect()
+					expected := []int{2, 3, 5, 7, 11, 13, 17, 19, 23, 29}
+
+					if !reflect.DeepEqual(primes, expected) {
+						panic(fmt.Sprintf("expected %#v to equal %#v", expected, primes))
+					}
+				}
+			`))
+
+			Expect(cmd.Run()).To(Succeed())
+		})
+	})
 })
