53eaea69949664ae0f823b4d069c3a7e4b24c6c3
Tom Godkin <thgodkin@gmail.com>
2018-07-10 20:49:37 +0100

    generate more canonically

--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
 	"os"
 	"path/filepath"
 
@@ -10,34 +8,50 @@ import (
 	"github.com/dave/jennifer/jen"
 )
 
+type generator struct {
+	name     string
+	generate func(string) *jen.File
+}
+
+func (g generator) do(typeName string) error {
+	src, err := os.Create(filepath.Join("f"+typeName, g.name))
+	if err != nil {
+		return err
+	}
+	defer src.Close()
+
+	return g.generate(typeName).Render(src)
+}
+
 func main() {
 	args, err := parseArgs()
-	if err != nil {
-		os.Exit(1)
-	}
+	exitOn(err)
 
 	err = os.Mkdir("f"+args.Positional.TypeName, 0755)
-	if err != nil {
-		os.Exit(1)
+	exitOn(err)
+
+	files := []generator{
+		{"drop.go", gen.Drop},
+		{"exclude.go", gen.Exclude},
+		{"filter.go", gen.Filter},
+		{"functor.go", gen.Functor},
+		{"iter.go", gen.Iter},
+		{"map.go", gen.Map},
+		{"option.go", gen.Option},
+		{"take.go", gen.Take},
 	}
 
-	files := map[string]func(string) *jen.File{
-		"drop.go":    gen.Drop,
-		"exclude.go": gen.Exclude,
-		"filter.go":  gen.Filter,
-		"functor.go": gen.Functor,
-		"iter.go":    gen.Iter,
-		"map.go":     gen.Map,
-		"option.go":  gen.Option,
-		"take.go":    gen.Take,
+	for _, g := range files {
+		err = g.do(args.Positional.TypeName)
+		exitOn(err)
+	}
+}
+
+func exitOn(err error) {
+	if err == nil {
+		return
 	}
 
-	for f, generate := range files {
-		path := filepath.Join("f"+args.Positional.TypeName, f)
-		content := fmt.Sprintf("%#v", generate(args.Positional.TypeName))
-		err = ioutil.WriteFile(path, []byte(content), 0755)
-		if err != nil {
-			os.Exit(1)
-		}
-	}
+	os.Stderr.WriteString(err.Error())
+	os.Exit(1)
 }
