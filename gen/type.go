145a9e435994188fe860f1c117d6eddc9fff787d
Tom Godkin <tgodkin@pivotal.io>
2018-07-26 17:27:51 +0100

    simplify type generation

--- gen/type.go
+++ gen/type.go
@@ -5,20 +5,13 @@ import "github.com/dave/jennifer/jen"
 func TypeFileContent(typeName string) *jen.File {
 	f := jen.NewFile("f" + typeName)
 
-	f.Type().Id("T").Id(typeName)
-	f.Line()
-
-	f.Type().Id("tSlice").Index().Id(typeName)
-	f.Line()
-
-	f.Type().Id("mapFunc").Func().Params(jen.Id(typeName)).Id(typeName)
-	f.Line()
-
-	f.Type().Id("foldFunc").Func().Params(jen.Id(typeName), jen.Id(typeName)).Id(typeName)
-	f.Line()
-
-	f.Type().Id("filterFunc").Func().Params(jen.Id(typeName)).Bool()
-	f.Line()
+	f.Type().Defs(
+		jen.Id("T").Id(typeName),
+		jen.Id("tSlice").Index().Id(typeName),
+		jen.Id("mapFunc").Func().Params(jen.Id(typeName)).Id(typeName),
+		jen.Id("foldFunc").Func().Params(jen.Id(typeName), jen.Id(typeName)).Id(typeName),
+		jen.Id("filterFunc").Func().Params(jen.Id(typeName)).Bool(),
+	)
 
 	f.Func().Id("fromT").Params(jen.Id("value").Id("T")).Id(typeName).Block(
 		jen.Return(jen.Id(typeName).Call(jen.Id("value"))),
