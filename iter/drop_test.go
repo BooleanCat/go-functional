547222525b1d2cc18c14e2ff5a68348d126b96be
Tom Godkin <tomgodkin@pm.me>
2022-02-11 23:53:42 +0000

    Add example specs for Take and Drop

--- iter/drop_test.go
+++ iter/drop_test.go
@@ -1,11 +1,18 @@
 package iter_test
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/BooleanCat/go-functional/internal/assert"
 	"github.com/BooleanCat/go-functional/iter"
 )
+
+func ExampleDrop() {
+	counter := iter.Drop[int](iter.Count(), 2)
+	fmt.Println(counter.Next().Unwrap())
+	// Output: 2
+}
 
 func TestDrop(t *testing.T) {
 	counter := iter.Drop[int](iter.Count(), 2)
--- iter/take_test.go
+++ iter/take_test.go
@@ -1,11 +1,23 @@
 package iter_test
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/BooleanCat/go-functional/internal/assert"
 	"github.com/BooleanCat/go-functional/iter"
 )
+
+func ExampleTake() {
+	iter := iter.Take[int](iter.Count(), 2)
+	fmt.Println(iter.Next())
+	fmt.Println(iter.Next())
+	fmt.Println(iter.Next())
+	// Output:
+	// Some(0)
+	// Some(1)
+	// None
+}
 
 func TestTakeIter(t *testing.T) {
 	iter := iter.Take[int](iter.Count(), 2)
