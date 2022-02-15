97c19b857683e8e71a457bd76905d5872332226f
Richard Pryce <richard.pryce@engineerbetter.com>
2022-02-15 22:31:45 +0000

    Add example tests

--- iter/map_test.go
+++ iter/map_test.go
@@ -1,12 +1,22 @@
 package iter_test
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/BooleanCat/go-functional/internal/assert"
 	"github.com/BooleanCat/go-functional/iter"
 )
 
+func ExampleMap() {
+	double := func(a int) int { return a * 2 }
+	items := iter.Collect[int](iter.Take[int](
+		iter.Map[int](iter.Count(), double),
+		4,
+	))
+	fmt.Println(items)
+	// Output: [0 2 4 6]
+}
 func TestMap(t *testing.T) {
 	double := func(a int) int { return a * 2 }
 	items := iter.Collect[int](iter.Take[int](
--- iter/zip_test.go
+++ iter/zip_test.go
@@ -2,11 +2,22 @@ package iter_test
 
 import (
 	"testing"
-
+	"fmt"
 	"github.com/BooleanCat/go-functional/internal/assert"
 	"github.com/BooleanCat/go-functional/iter"
 )
 
+func ExampleZip() {
+	isEven := func(a int) bool { return a%2 == 0 }
+	evens := iter.Filter[int](iter.Count(), isEven)
+	odds := iter.Exclude[int](iter.Count(), isEven)
+
+	zipped := iter.Collect[iter.Tuple[int, int]](
+		iter.Take[iter.Tuple[int, int]](iter.Zip[int, int](evens, odds), 3),
+	)
+	fmt.Println(zipped)
+	//output: [{0 1} {2 3} {4 5}]
+}
 func TestZip(t *testing.T) {
 	isEven := func(a int) bool { return a%2 == 0 }
 	evens := iter.Filter[int](iter.Count(), isEven)
