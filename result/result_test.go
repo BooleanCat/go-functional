87c7a799675b4889dd60f88ae5c10ecefdbe0853
Tom Godkin <tomgodkin@pm.me>
2022-02-19 21:21:24 +0000

    Add example tests for result

--- result/result_test.go
+++ result/result_test.go
@@ -9,6 +9,70 @@ import (
 	"github.com/BooleanCat/go-functional/result"
 )
 
+func ExampleResult_Unwrap() {
+	fmt.Println(result.Ok(4).Unwrap())
+	// Output: 4
+}
+
+func ExampleResult_UnwrapOr() {
+	fmt.Println(result.Ok(4).UnwrapOr(3))
+	fmt.Println(result.Err[int](errors.New("oops")).UnwrapOr(3))
+	// Output:
+	// 4
+	// 3
+}
+
+func ExampleResult_UnwrapOrElse() {
+	fmt.Println(result.Ok(4).UnwrapOrElse(func() int {
+		return 3
+	}))
+
+	fmt.Println(result.Err[int](errors.New("oops")).UnwrapOrElse(func() int {
+		return 3
+	}))
+
+	// Output:
+	// 4
+	// 3
+}
+
+func ExampleResult_UnwrapOrZero() {
+	fmt.Println(result.Ok(4).UnwrapOrZero())
+	fmt.Println(result.Err[int](errors.New("oops")).UnwrapOrZero())
+
+	// Output
+	// 4
+	// 0
+}
+
+func ExampleResult_IsOk() {
+	fmt.Println(result.Ok(4).IsOk())
+	fmt.Println(result.Err[int](errors.New("oops")).IsOk())
+
+	// Output:
+	// true
+	// false
+}
+
+func ExampleResult_IsErr() {
+	fmt.Println(result.Ok(4).IsErr())
+	fmt.Println(result.Err[int](errors.New("oops")).IsErr())
+
+	// Output:
+	// false
+	// true
+}
+
+func ExampleResult_Value() {
+	value, ok := result.Ok(4).Value()
+	fmt.Println(value)
+	fmt.Println(ok)
+
+	// Output:
+	// 4
+	// <nil>
+}
+
 func TestOkStringer(t *testing.T) {
 	assert.Equal(t, fmt.Sprintf("%s", result.Ok(42)), "Ok(42)")
 }
