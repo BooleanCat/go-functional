2e842b2f2be6cf3b668d190c20c367b03a4f5097
Tom Godkin <tomgodkin@pm.me>
2023-08-10 13:19:03 +0100

    Minor refactor of docstring

--- iter/find.go
+++ iter/find.go
@@ -2,9 +2,8 @@ package iter
 
 import "github.com/BooleanCat/go-functional/option"
 
-// Find searches for the first occurance of a value that satisfies the
-// predicate and returns that value. If no value satisfies the predicate, it
-// returns `None`.
+// Find the first occurance of a value that satisfies the predicate and return
+// that value. If no value satisfies the predicate, return `None`.
 func Find[T any](iter Iterator[T], predicate func(v T) bool) option.Option[T] {
 	for {
 		if value, ok := iter.Next().Value(); ok {
