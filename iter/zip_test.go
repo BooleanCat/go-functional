1e497c960c9165dafa86a224f5a77ee41acbb911
Tom Godkin <tomgodkin@pm.me>
2023-08-14 13:03:08 +0100

    Improve test coverage in zip.go

--- iter/zip_test.go
+++ iter/zip_test.go
@@ -31,3 +31,23 @@ func TestZip(t *testing.T) {
 
 	assert.SliceEqual(t, zipped, []iter.Tuple[int, int]{{0, 1}, {2, 3}, {4, 5}})
 }
+
+func TestZipFirstExhausted(t *testing.T) {
+	isEven := func(a int) bool { return a%2 == 0 }
+	evens := iter.Take[int](iter.Filter[int](iter.Count(), isEven), 2)
+	odds := iter.Exclude[int](iter.Count(), isEven)
+
+	zipped := iter.Collect[iter.Tuple[int, int]](iter.Zip[int, int](evens, odds))
+
+	assert.SliceEqual(t, zipped, []iter.Tuple[int, int]{{0, 1}, {2, 3}})
+}
+
+func TestZipSecondExhausted(t *testing.T) {
+	isEven := func(a int) bool { return a%2 == 0 }
+	evens := iter.Filter[int](iter.Count(), isEven)
+	odds := iter.Take[int](iter.Exclude[int](iter.Count(), isEven), 2)
+
+	zipped := iter.Collect[iter.Tuple[int, int]](iter.Zip[int, int](evens, odds))
+
+	assert.SliceEqual(t, zipped, []iter.Tuple[int, int]{{0, 1}, {2, 3}})
+}
