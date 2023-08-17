3024bff1887ecd032a6e7b2ce9a0a079db0d62bf
Tom Godkin <tomgodkin@pm.me>
2023-08-17 18:02:06 +0100

    Add cycle iter

---
+++ iter/cycle.go
@@ -0,0 +1,50 @@
+package iter
+
+import "github.com/BooleanCat/go-functional/option"
+
+// CycleIter implements `Cycle`. See `Cycle`'s documentation.
+type CycleIter[T any] struct {
+	iter  Iterator[T]
+	items []T
+	index int
+}
+
+// Cycle instantiates a `CycleIter` yielding all items from the provided
+// iterator until exhaustion, and then yields them all over again on repeat.
+//
+// Note that CycleIter stores the members from the underlying iterator so will
+// grow in size as items are yielded until the underlying iterator is
+// exhausted.
+//
+// In most cases this iterator is infinite, except when the underlying iterator
+// is exhausted before the first call to Next() in which case this iterator
+// will always yield None.
+func Cycle[T any](iter Iterator[T]) *CycleIter[T] {
+	return &CycleIter[T]{iter, make([]T, 0), 0}
+}
+
+// Next implements the Iterator interface for `CycleIter`.
+func (iter *CycleIter[T]) Next() option.Option[T] {
+	if iter.iter != nil {
+		if value, ok := iter.iter.Next().Value(); ok {
+			iter.items = append(iter.items, value)
+			return option.Some(value)
+		} else {
+			iter.iter = nil
+		}
+	}
+
+	if len(iter.items) == 0 {
+		return option.None[T]()
+	}
+
+	if iter.index == len(iter.items) {
+		iter.index = 0
+	}
+
+	next := iter.items[iter.index]
+	iter.index++
+	return option.Some(next)
+}
+
+var _ Iterator[struct{}] = new(CycleIter[struct{}])
---
+++ iter/cycle_test.go
@@ -0,0 +1,40 @@
+package iter_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/BooleanCat/go-functional/internal/assert"
+	"github.com/BooleanCat/go-functional/internal/fakes"
+	"github.com/BooleanCat/go-functional/iter"
+)
+
+func ExampleCycle() {
+	numbers := iter.Take[int](iter.Cycle[int](iter.Lift([]int{1, 2})), 5)
+	fmt.Println(numbers.Collect())
+	// Output: [1 2 1 2 1]
+}
+
+func TestCycleIter(t *testing.T) {
+	items := iter.Take[int](iter.Cycle[int](iter.Lift([]int{1, 2, 3})), 2).Collect()
+	assert.SliceEqual(t, items, []int{1, 2})
+}
+
+func TestCycleIterOverflow(t *testing.T) {
+	items := iter.Take[int](iter.Cycle[int](iter.Lift([]int{1, 2})), 5).Collect()
+	assert.SliceEqual(t, items, []int{1, 2, 1, 2, 1})
+}
+
+func TestCycleIterEmpty(t *testing.T) {
+	items := iter.Cycle[int](iter.Exhausted[int]())
+	assert.True(t, items.Next().IsNone())
+}
+
+func TestCycleExhausted(t *testing.T) {
+	delegate := new(fakes.Iterator[int])
+	ones := iter.Cycle[int](delegate)
+
+	assert.True(t, ones.Next().IsNone())
+	assert.True(t, ones.Next().IsNone())
+	assert.Equal(t, delegate.NextCallCount(), 1)
+}
