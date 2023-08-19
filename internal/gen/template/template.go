501fc913420f862a4e562d3ffeaeee6a927c4dfd
Tom Godkin <tomgodkin@pm.me>
2023-08-19 12:28:33 +0100

    Add more method templates

--- internal/gen/template/template.go
+++ internal/gen/template/template.go
@@ -1,6 +1,34 @@
 package template
 
-import "github.com/BooleanCat/go-functional/iter"
+import (
+	"bytes"
+	_ "embed"
+	"fmt"
+	"text/template"
+
+	"github.com/BooleanCat/go-functional/iter"
+	"github.com/BooleanCat/go-functional/result"
+)
+
+var (
+	//go:embed Header.go.tmpl
+	HeaderTemplate []byte
+
+	//go:embed Drop.go.tmpl
+	DropTemplate string
+
+	//go:embed Take.go.tmpl
+	TakeTemplate string
+
+	//go:embed ForEach.go.tmpl
+	ForEachTemplate string
+)
+
+var templates = map[string]string{
+	"Drop":    DropTemplate,
+	"Take":    TakeTemplate,
+	"ForEach": ForEachTemplate,
+}
 
 type Values struct {
 	TypeName   string
@@ -11,4 +39,23 @@ func FromMethodSetItems(items map[string]string) []Values {
 	return iter.Map[iter.Tuple[string, string], Values](iter.LiftHashMap(items), func(item iter.Tuple[string, string]) Values {
 		return Values{TypeName: item.One, ReturnType: item.Two}
 	}).Collect()
+}
+
+func RenderTemplate(name string, values []Values) result.Result[[]byte] {
+	targetTemplate, ok := templates[name]
+	if !ok {
+		return result.Err[[]byte](fmt.Errorf("invalid template: %s", name))
+	}
+
+	tmpl, err := template.New(name).Parse(targetTemplate)
+	if err != nil {
+		return result.Err[[]byte](err)
+	}
+
+	buffer := new(bytes.Buffer)
+	if err := tmpl.Execute(buffer, values); err != nil {
+		result.Err[[]byte](err)
+	}
+
+	return result.Ok(buffer.Bytes())
 }
