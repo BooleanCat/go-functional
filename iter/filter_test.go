60cf958ccfc3e5795790f5050ad6946a2b5ea694
William-Young-97 <wadyoung@outlook.com>
2023-08-22 22:50:14 +0100

    Add TestFilterFilter

--- iter/filter_test.go
+++ iter/filter_test.go
@@ -64,6 +64,11 @@ func TestFilter_method(t *testing.T) {
 	assert.Equal(t, evens.Next().Unwrap(), 2)
 }
 
+func TestFilterFilter(t *testing.T) {
+	evens := iter.Count().Filter(filters.IsEven[int]).Filter(filters.GreaterThan[int](1))
+	assert.Equal(t, evens.Next().Unwrap(), 2)
+}
+
 func TestFilterEmpty(t *testing.T) {
 	evens := iter.Filter[int](iter.Exhausted[int](), filters.IsEven[int])
 	assert.True(t, evens.Next().IsNone())
