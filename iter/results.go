ece50cdf941d64d8384d982a9e1c4ee62a757605
Tom Godkin <tomgodkin@pm.me>
2023-08-26 16:12:51 +0100

    Fix CollectResults doc comment

--- iter/results.go
+++ iter/results.go
@@ -5,6 +5,9 @@ import "github.com/BooleanCat/go-functional/result"
 // CollectResults consumes an [Iterator] with a yield type of [result.Result].
 // It returns all items as a slice. It does not protect against infinite
 // iterators.
+//
+// When a [result.Err] is encountered, collection stops and the [result.Err] is
+// returned immediately.
 func CollectResults[T any](iter Iterator[result.Result[T]]) result.Result[[]T] {
 	values := make([]T, 0)
 
