2d2a861b3a873b4f7ddc208cbdf7212947bd7582
Rahul Chocha <rahulahir4530@gmail.com>
2023-09-02 11:43:03 +0530

    feat: added test cases to improve coverage

--- iter/zip_test.go
+++ iter/zip_test.go
@@ -24,9 +24,11 @@ func TestPairStringer(t *testing.T) {
 	}
 	pair1 := iter.Pair[string, interface{}]{One: "1", Two: foo}
 	pair2 := iter.Pair[int, interface{}]{One: 2, Two: pair1}
+	pair3 := iter.Pair[interface{}, interface{}]{One: pair1, Two: pair2}
 
 	assert.Equal(t, pair1.String(), "(1, map[text:Random Text])")
 	assert.Equal(t, pair2.String(), "(2, (1, map[text:Random Text]))")
+	assert.Equal(t, pair3.String(), "((1, map[text:Random Text]), (2, (1, map[text:Random Text])))")
 }
 
 func TestZip(t *testing.T) {
