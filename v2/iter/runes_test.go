44de9856f01fd86785db254ceb492594c692f4fc
Tom Godkin <tomgodkin@pm.me>
2024-02-03 11:54:16 +0000

    Add Runes to v2

---
+++ v2/iter/runes_test.go
@@ -0,0 +1,38 @@
+package iter_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/BooleanCat/go-functional/v2/internal/assert"
+	"github.com/BooleanCat/go-functional/v2/iter"
+)
+
+func ExampleRunes() {
+	runes := iter.Runes("Hello, 世界!").Collect()
+
+	fmt.Println(string(runes[7:9]))
+	// Output: 世界
+}
+
+func ExampleRunes_slice() {
+	runes := iter.Runes([]rune("Hello, 世界!")).Collect()
+
+	fmt.Println(string(runes[7:9]))
+	// Output: 世界
+}
+
+func TestRunes(t *testing.T) {
+	runes := iter.Runes("Hello, 世界!").Collect()
+	assert.SliceEqual[rune](t, runes, []rune{'H', 'e', 'l', 'l', 'o', ',', ' ', '世', '界', '!'})
+}
+
+func TestRunesSlice(t *testing.T) {
+	runes := iter.Runes([]rune("Hello, 世界!")).Collect()
+	assert.SliceEqual[rune](t, runes, []rune{'H', 'e', 'l', 'l', 'o', ',', ' ', '世', '界', '!'})
+}
+
+func TestRunesEmpty(t *testing.T) {
+	runes := iter.Runes("").Collect()
+	assert.Empty[rune](t, runes)
+}
