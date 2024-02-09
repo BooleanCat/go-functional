6e4b97bad80c6e8209c8448ec402267c74145cc7
Tom Godkin <tomgodkin@pm.me>
2024-02-09 12:34:54 +0000

    Implement Zip

--- iter/iter_test.go
+++ iter/iter_test.go
@@ -97,3 +97,29 @@ func TestLiftHashMapTerminateEarly(t *testing.T) {
 	_, stop := it.Pull2(it.Seq2[int, string](iter.LiftHashMap(map[int]string{1: "one", 2: "two", 3: "three"})))
 	stop()
 }
+
+func ExampleZip() {
+	for left, right := range iter.Zip(iter.Lift([]int{1, 2, 3}), iter.Lift([]string{"one", "two", "three"})) {
+		fmt.Println(left, right)
+	}
+
+	// Output:
+	// 1 one
+	// 2 two
+	// 3 three
+}
+
+func TestZipEmpty(t *testing.T) {
+	t.Parallel()
+
+	for _, _ = range iter.Zip(iter.Lift([]int{}), iter.Lift([]string{})) {
+		t.Error("unexpected")
+	}
+}
+
+func TestZipTerminateEarly(t *testing.T) {
+	t.Parallel()
+
+	_, stop := it.Pull2(it.Seq2[int, string](iter.Zip(iter.Lift([]int{1, 2}), iter.Lift([]string{"one", "two"}))))
+	stop()
+}
