69caca7754bb1cf7764d481b0dbc1ef6ccb11b55
Tom Godkin <tomgodkin@pm.me>
2024-02-09 12:34:54 +0000

    Implement Zip

--- iter/iter.go
+++ iter/iter.go
@@ -51,3 +51,27 @@ func LiftHashMap[K comparable, V any](m map[K]V) Iterator2[K, V] {
 		}
 	}))
 }
+
+// Zip yields pairs of values from two iterators.
+func Zip[V, W any](left Iterator[V], right Iterator[W]) Iterator2[V, W] {
+	return Iterator2[V, W](iter.Seq2[V, W](func(yield func(V, W) bool) {
+		left, stop := iter.Pull(iter.Seq[V](left))
+		defer stop()
+
+		right, stop := iter.Pull(iter.Seq[W](right))
+		defer stop()
+
+		for {
+			leftValue, leftOk := left()
+			rightValue, rightOk := right()
+
+			if !leftOk || !rightOk {
+				return
+			}
+
+			if !yield(leftValue, rightValue) {
+				return
+			}
+		}
+	}))
+}
