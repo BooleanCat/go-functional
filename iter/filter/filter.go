a0e0a3f53cf1b2552875c17486bdff5b48e78680
Tom Godkin <tomgodkin@pm.me>
2024-02-10 18:09:10 +0000

    Implement more filters

--- iter/filter/filter.go
+++ iter/filter/filter.go
@@ -1,6 +1,8 @@
 // This package contains functions intended for use with [iter.Filter].
 package filter
 
+import "cmp"
+
 // IsEven returns true when the provided integer is even.
 func IsEven[T ~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 | ~uintptr | ~int | ~int8 | ~int16 | ~int32 | ~int64](integer T) bool {
 	return integer%2 == 0
@@ -9,4 +11,69 @@ func IsEven[T ~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 | ~uintptr | ~int | ~
 // IsOdd returns true when the provided integer is odd.
 func IsOdd[T ~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 | ~uintptr | ~int | ~int8 | ~int16 | ~int32 | ~int64](integer T) bool {
 	return integer%2 != 0
+}
+
+// IsEqual returns a function that returns true when the provided value is equal
+// to some value.
+func IsEqual[T comparable](value T) func(T) bool {
+	return func(candidate T) bool {
+		return candidate == value
+	}
+}
+
+// IsZero returns true when the provided value is the zero value for its type.
+func IsZero[T comparable](value T) bool {
+	var zero T
+	return value == zero
+}
+
+// GreaterThan returns a function that returns true when the provided value is
+// greater than a threshold.
+func GreaterThan[T cmp.Ordered](threshold T) func(T) bool {
+	return func(value T) bool {
+		return value > threshold
+	}
+}
+
+// LessThan returns a function that returns true when the provided value is less
+// than a threshold.
+func LessThan[T cmp.Ordered](threshold T) func(T) bool {
+	return func(value T) bool {
+		return value < threshold
+	}
+}
+
+// Not returns a function that inverts the result of the provided function.
+func Not[T any](fn func(T) bool) func(T) bool {
+	return func(value T) bool {
+		return !fn(value)
+	}
+}
+
+// And returns a function that returns true when all provided functions return
+// true.
+func And[T any](filters ...func(T) bool) func(T) bool {
+	return func(value T) bool {
+		for _, filter := range filters {
+			if !filter(value) {
+				return false
+			}
+		}
+
+		return true
+	}
+}
+
+// Or returns a function that returns true when any of the provided functions
+// return true.
+func Or[T any](filters ...func(T) bool) func(T) bool {
+	return func(value T) bool {
+		for _, filter := range filters {
+			if filter(value) {
+				return true
+			}
+		}
+
+		return false
+	}
 }
--- iter/filter/filter_test.go
+++ iter/filter/filter_test.go
@@ -16,3 +16,38 @@ func ExampleIsOdd() {
 	fmt.Println(iter.Lift([]int{1, 2, 3, 4}).Filter(filter.IsOdd).Collect())
 	// Output: [1 3]
 }
+
+func ExampleIsEqual() {
+	fmt.Println(iter.Lift([]int{1, 2, 3, 4}).Filter(filter.IsEqual(2)).Collect())
+	// Output: [2]
+}
+
+func ExampleIsZero() {
+	fmt.Println(iter.Lift([]int{0, 1, 2, 3}).Filter(filter.IsZero).Collect())
+	// Output: [0]
+}
+
+func ExampleGreaterThan() {
+	fmt.Println(iter.Lift([]int{1, 2, 3, 4}).Filter(filter.GreaterThan(2)).Collect())
+	// Output: [3 4]
+}
+
+func ExampleLessThan() {
+	fmt.Println(iter.Lift([]int{1, 2, 3, 4}).Filter(filter.LessThan(3)).Collect())
+	// Output: [1 2]
+}
+
+func ExampleNot() {
+	fmt.Println(iter.Lift([]int{1, 2, 3, 4}).Filter(filter.Not[int](filter.IsEven)).Collect())
+	// Output: [1 3]
+}
+
+func ExampleAnd() {
+	fmt.Println(iter.Lift([]int{1, 2, 3, 4}).Filter(filter.And(filter.IsOdd, filter.GreaterThan(2))).Collect())
+	// Output: [3]
+}
+
+func ExampleOr() {
+	fmt.Println(iter.Lift([]int{1, 2, 3, 4}).Filter(filter.Or(filter.IsEven, filter.LessThan(3))).Collect())
+	// Output: [1 2 4]
+}
