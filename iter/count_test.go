72f8409e9c084868e3ff7b0db69df19c9dc93d46
Tom Godkin <tomgodkin@pm.me>
2024-02-17 10:46:37 +0000

    Add Count iterator

---
+++ iter/count_test.go
@@ -0,0 +1,31 @@
+package iter_test
+
+import (
+	"fmt"
+	it "iter"
+	"testing"
+
+	"github.com/BooleanCat/go-functional/v2/iter"
+)
+
+func ExampleCount() {
+	for i := range iter.Count() {
+		if i >= 3 {
+			break
+		}
+
+		fmt.Println(i)
+	}
+
+	// Output:
+	// 0
+	// 1
+	// 2
+}
+
+func TestCountTerminateEarly(t *testing.T) {
+	t.Parallel()
+
+	_, stop := it.Pull(it.Seq[int](iter.Count()))
+	stop()
+}
