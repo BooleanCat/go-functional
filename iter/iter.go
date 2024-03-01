0f5196b53661b40e851d731fe17d52bcfeb28d87
Tom Godkin <tomgodkin@pm.me>
2024-03-01 12:43:18 +0000

    Add ForEach and Reduce consumers

--- iter/iter.go
+++ iter/iter.go
@@ -51,3 +51,51 @@ func LiftHashMap[K comparable, V any](m map[K]V) Iterator2[K, V] {
 		}
 	}))
 }
+
+// ForEach consumes an iterator and applies a function to each value yielded.
+func ForEach[V any](iter Iterator[V], fn func(V)) {
+	for item := range iter {
+		fn(item)
+	}
+}
+
+// ForEach is a convenience method for chaining [ForEach] on [Iterator]s.
+func (iter Iterator[V]) ForEach(fn func(V)) {
+	ForEach[V](iter, fn)
+}
+
+// ForEach2 consumes an iterator and applies a function to each pair of values.
+func ForEach2[V, W any](iter Iterator2[V, W], fn func(V, W)) {
+	for v, w := range iter {
+		fn(v, w)
+	}
+}
+
+// ForEach2 is a convenience method for chaining [ForEach2] on [Iterator2]s.
+func (iter Iterator2[V, W]) ForEach2(fn func(V, W)) {
+	ForEach2[V, W](iter, fn)
+}
+
+// Reduce consumes an iterator and applies a function to each value yielded,
+// accumulating a single result.
+func Reduce[V any, R any](iter Iterator[V], fn func(R, V) R, initial R) R {
+	result := initial
+
+	for item := range iter {
+		result = fn(result, item)
+	}
+
+	return result
+}
+
+// Reduce2 consumes an iterator and applies a function to each pair of values,
+// accumulating a single result.
+func Reduce2[V, W any, R any](iter Iterator2[V, W], fn func(R, V, W) R, initial R) R {
+	result := initial
+
+	for v, w := range iter {
+		result = fn(result, v, w)
+	}
+
+	return result
+}
--- iter/iter_test.go
+++ iter/iter_test.go
@@ -97,3 +97,81 @@ func TestLiftHashMapTerminateEarly(t *testing.T) {
 	_, stop := it.Pull2(it.Seq2[int, string](iter.LiftHashMap(map[int]string{1: "one", 2: "two", 3: "three"})))
 	stop()
 }
+
+func ExampleForEach() {
+	iter.ForEach(iter.Lift([]int{1, 2, 3}), func(number int) {
+		fmt.Println(number)
+	})
+	// Output:
+	// 1
+	// 2
+	// 3
+}
+
+func ExampleForEach_method() {
+	iter.Lift([]int{1, 2, 3}).ForEach(func(number int) {
+		fmt.Println(number)
+	})
+	// Output:
+	// 1
+	// 2
+	// 3
+}
+
+func TestForEachEmpty(t *testing.T) {
+	t.Parallel()
+
+	iter.ForEach(iter.Lift([]int{}), func(int) {
+		t.Error("unexpected")
+	})
+}
+
+func ExampleForEach2() {
+	iter.ForEach2(iter.Lift([]int{1, 2, 3}).Enumerate(), func(index int, number int) {
+		fmt.Println(index, number)
+	})
+	// Output:
+	// 0 1
+	// 1 2
+	// 2 3
+}
+
+func ExampleForEach2_method() {
+	iter.Lift([]int{1, 2, 3}).Enumerate().ForEach2(func(index int, number int) {
+		fmt.Println(index, number)
+	})
+	// Output:
+	// 0 1
+	// 1 2
+	// 2 3
+}
+
+func TestForEach2Empty(t *testing.T) {
+	t.Parallel()
+
+	iter.ForEach2(iter.Lift([]int{}).Enumerate(), func(int, int) {
+		t.Error("unexpected")
+	})
+}
+
+func ExampleReduce() {
+	fmt.Println(iter.Reduce(iter.Lift([]int{1, 2, 3}), func(a, b int) int {
+		return a + b
+	}, 0))
+
+	// Output: 6
+}
+
+func TestReduceEmpty(t *testing.T) {
+	t.Parallel()
+
+	assert.Equal(t, iter.Reduce(iter.Lift([]int{}), func(int, int) int { return 0 }, 0), 0)
+}
+
+func ExampleReduce2() {
+	fmt.Println(iter.Reduce2(iter.Lift([]int{1, 2, 3}).Enumerate(), func(i, a, b int) int {
+		return i + 1
+	}, 0))
+
+	// Output: 3
+}
