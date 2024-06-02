5b5e9ac275d354c7dfa67b55cfbda10b3546e86e
Tom Godkin <tomgodkin@pm.me>
2024-06-02 17:04:47 +0100

    Add Cycle iterator

---
+++ iter/cycle_test.go
@@ -0,0 +1,53 @@
+package iter_test
+
+import (
+	"fmt"
+	it "iter"
+	"testing"
+
+	"github.com/BooleanCat/go-functional/v2/future/maps"
+	"github.com/BooleanCat/go-functional/v2/future/slices"
+	"github.com/BooleanCat/go-functional/v2/iter"
+)
+
+func ExampleCycle() {
+	numbers := slices.Collect(iter.Take(iter.Cycle(slices.Values([]int{1, 2})), 5))
+
+	fmt.Println(numbers)
+	// Output: [1 2 1 2 1]
+}
+
+func ExampleCycle_method() {
+	numbers := iter.Iterator[int](slices.Values([]int{1, 2})).Cycle().Take(5).Collect()
+
+	fmt.Println(numbers)
+	// Output: [1 2 1 2 1]
+}
+
+func TestCycleTerminateEarly(t *testing.T) {
+	t.Parallel()
+
+	_, stop := it.Pull(iter.Cycle(slices.Values([]int{1, 2})))
+	stop()
+}
+
+func ExampleCycle2() {
+	numbers := maps.Collect(iter.Take2(iter.Cycle2(maps.All(map[int]string{1: "one"})), 5))
+
+	fmt.Println(numbers)
+	// Output: map[1:one]
+}
+
+func ExampleCycle2_method() {
+	numbers := iter.Iterator2[int, string](maps.All(map[int]string{1: "one"})).Cycle().Take(5)
+
+	fmt.Println(maps.Collect(it.Seq2[int, string](numbers)))
+	// Output: map[1:one]
+}
+
+func TestCycle2TerminateEarly(t *testing.T) {
+	t.Parallel()
+
+	_, stop := it.Pull2(iter.Cycle2(maps.All(map[int]string{1: "one"})))
+	stop()
+}
