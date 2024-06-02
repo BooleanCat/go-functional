ad36319647b41de8eff701ff815efc90b1833599
Tom Godkin <tomgodkin@pm.me>
2024-06-02 09:40:59 +0100

    Add Take2 iterator

--- iter/take.go
+++ iter/take.go
@@ -26,3 +26,28 @@ func Take[V any](delegate iter.Seq[V], limit int) iter.Seq[V] {
 func (iterator Iterator[V]) Take(limit int) Iterator[V] {
 	return Iterator[V](Take(iter.Seq[V](iterator), limit))
 }
+
+// Take2 yields the first `limit` pairs of values from a delegate [Iterator2].
+func Take2[V, W any](delegate iter.Seq2[V, W], limit int) iter.Seq2[V, W] {
+	return func(yield func(V, W) bool) {
+		if limit <= 0 {
+			return
+		}
+
+		for v, w := range delegate {
+			if !yield(v, w) {
+				return
+			}
+
+			limit--
+			if limit <= 0 {
+				return
+			}
+		}
+	}
+}
+
+// Take is a convenience method for chaining [Take] on [Iterator2]s.
+func (iterator Iterator2[V, W]) Take(limit int) Iterator2[V, W] {
+	return Iterator2[V, W](Take2(iter.Seq2[V, W](iterator), limit))
+}
--- iter/take_test.go
+++ iter/take_test.go
@@ -3,8 +3,10 @@ package iter_test
 import (
 	"fmt"
 	it "iter"
+	sl "slices"
 	"testing"
 
+	"github.com/BooleanCat/go-functional/v2/future/maps"
 	"github.com/BooleanCat/go-functional/v2/future/slices"
 	"github.com/BooleanCat/go-functional/v2/internal/assert"
 	"github.com/BooleanCat/go-functional/v2/iter"
@@ -49,4 +51,54 @@ func TestTakeEmpty(t *testing.T) {
 	t.Parallel()
 
 	assert.Empty[int](t, slices.Collect(iter.Take(slices.Values([]int{}), 2)))
+}
+
+func ExampleTake2() {
+	numbers := maps.Collect(iter.Take2(maps.All(map[int]string{1: "one", 2: "two", 3: "three"}), 2))
+
+	fmt.Println(len(numbers))
+	// Output: 2
+}
+
+func ExampleTake2_method() {
+	numbers := maps.Collect(it.Seq2[int, string](iter.Iterator2[int, string](maps.All(map[int]string{1: "one", 2: "two", 3: "three"})).Take(2)))
+
+	fmt.Println(len(numbers))
+	// Output: 2
+}
+
+func TestTake2(t *testing.T) {
+	t.Parallel()
+
+	keys := []int{1, 2, 3}
+	values := []string{"one", "two", "three"}
+
+	numbers := maps.Collect(iter.Take2(iter.Zip(slices.Values(keys), slices.Values(values)), 2))
+
+	assert.Equal(t, len(numbers), 2)
+
+	for key := range numbers {
+		assert.True(t, sl.Contains(keys, key))
+	}
+}
+
+func TestTake2Zero(t *testing.T) {
+	t.Parallel()
+
+	numbers := maps.Collect(iter.Take2(maps.All(map[int]string{1: "one", 2: "two", 3: "three"}), 0))
+	assert.Equal(t, len(numbers), 0)
+}
+
+func TestTake2Empty(t *testing.T) {
+	t.Parallel()
+
+	numbers := maps.Collect(iter.Take2(maps.All(map[int]string{}), 2))
+	assert.Equal(t, len(numbers), 0)
+}
+
+func TestTake2TerminateEarly(t *testing.T) {
+	t.Parallel()
+
+	_, stop := it.Pull2(iter.Take2(maps.All(map[int]string{1: "one", 2: "two"}), 1))
+	stop()
 }
