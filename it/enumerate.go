1f695729cc3e5b9d18f90f954a2448f181f51015
Tom Godkin <tomgodkin@pm.me>
2024-06-30 22:21:22 +0100

    Improve performance of Enumerate iter

--- it/enumerate.go
+++ it/enumerate.go
@@ -5,15 +5,14 @@ import "iter"
 // Enumerate yields pairs of indices and values from an iterator.
 func Enumerate[V any](delegate iter.Seq[V]) iter.Seq2[int, V] {
 	return func(yield func(int, V) bool) {
-		delegate, stop := iter.Pull(delegate)
-		defer stop()
+		count := 0
 
-		for i := 0; ; i++ {
-			value, ok := delegate()
-
-			if !ok || !yield(i, value) {
+		for value := range delegate {
+			if !yield(count, value) {
 				return
 			}
+
+			count++
 		}
 	}
 }
