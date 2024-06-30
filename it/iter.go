000c09616bd7e348fc31185e9a7e299185df4013
Tom Godkin <tomgodkin@pm.me>
2024-06-30 20:51:21 +0100

    Add Max and Min consumers

--- it/iter.go
+++ it/iter.go
@@ -1,6 +1,9 @@
 package it
 
-import "iter"
+import (
+	"cmp"
+	"iter"
+)
 
 // ForEach consumes an iterator and applies a function to each value yielded.
 func ForEach[V any](iter iter.Seq[V], fn func(V)) {
@@ -38,4 +41,58 @@ func Reduce2[V, W any, R any](iter iter.Seq2[V, W], fn func(R, V, W) R, initial 
 	}
 
 	return result
+}
+
+// Max consumes an iterator and returns the maximum value yielded and true if
+// there was at least one value, or the zero value and false if the iterator
+// was empty.
+func Max[V cmp.Ordered](iterator iter.Seq[V]) (V, bool) {
+	var (
+		max V
+		set bool
+	)
+
+	next, _ := iter.Pull(iterator)
+	first, ok := next()
+	if !ok {
+		return max, set
+	}
+
+	max = first
+	set = true
+
+	for item := range iterator {
+		if item > max {
+			max = item
+		}
+	}
+
+	return max, set
+}
+
+// Min consumes an iterator and returns the minimum value yielded and true if
+// there was at least one value, or the zero value and false if the iterator
+// was empty.
+func Min[V cmp.Ordered](iterator iter.Seq[V]) (V, bool) {
+	var (
+		min V
+		set bool
+	)
+
+	next, _ := iter.Pull(iterator)
+	first, ok := next()
+	if !ok {
+		return min, set
+	}
+
+	min = first
+	set = true
+
+	for item := range iterator {
+		if item < min {
+			min = item
+		}
+	}
+
+	return min, set
 }
