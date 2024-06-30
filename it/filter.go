35de6f04816002612bccc3887bad97cee21c8658
Tom Godkin <tomgodkin@pm.me>
2024-06-30 22:19:56 +0100

    Improve performance of Filter iters

--- it/filter.go
+++ it/filter.go
@@ -1,22 +1,15 @@
 package it
 
-import (
-	"iter"
-
-	"github.com/BooleanCat/go-functional/v2/it/filter"
-)
+import "iter"
 
 // Filter yields values from an iterator that satisfy a predicate.
 func Filter[V any](delegate iter.Seq[V], predicate func(V) bool) iter.Seq[V] {
 	return func(yield func(V) bool) {
-		delegate, stop := iter.Pull(delegate)
-		defer stop()
-
-		for {
-			value, ok := delegate()
-
-			if !ok || predicate(value) && !yield(value) {
-				return
+		for value := range delegate {
+			if predicate(value) {
+				if !yield(value) {
+					return
+				}
 			}
 		}
 	}
@@ -24,20 +17,25 @@ func Filter[V any](delegate iter.Seq[V], predicate func(V) bool) iter.Seq[V] {
 
 // Exclude yields values from an iterator that do not satisfy a predicate.
 func Exclude[V any](delegate iter.Seq[V], predicate func(V) bool) iter.Seq[V] {
-	return Filter[V](delegate, filter.Not[V](predicate))
+	return func(yield func(V) bool) {
+		for value := range delegate {
+			if !predicate(value) {
+				if !yield(value) {
+					return
+				}
+			}
+		}
+	}
 }
 
 // Filter2 yields values from an iterator that satisfy a predicate.
 func Filter2[V, W any](delegate iter.Seq2[V, W], predicate func(V, W) bool) iter.Seq2[V, W] {
 	return func(yield func(V, W) bool) {
-		delegate, stop := iter.Pull2(delegate)
-		defer stop()
-
-		for {
-			left, right, ok := delegate()
-
-			if !ok || predicate(left, right) && !yield(left, right) {
-				return
+		for k, v := range delegate {
+			if predicate(k, v) {
+				if !yield(k, v) {
+					return
+				}
 			}
 		}
 	}
@@ -46,14 +44,11 @@ func Filter2[V, W any](delegate iter.Seq2[V, W], predicate func(V, W) bool) iter
 // Exclude2 yields values from an iterator that do not satisfy a predicate.
 func Exclude2[V, W any](delegate iter.Seq2[V, W], predicate func(V, W) bool) iter.Seq2[V, W] {
 	return func(yield func(V, W) bool) {
-		delegate, stop := iter.Pull2(delegate)
-		defer stop()
-
-		for {
-			left, right, ok := delegate()
-
-			if !ok || !predicate(left, right) && !yield(left, right) {
-				return
+		for k, v := range delegate {
+			if !predicate(k, v) {
+				if !yield(k, v) {
+					return
+				}
 			}
 		}
 	}
--- it/itx/filter.go
+++ it/itx/filter.go
@@ -8,17 +8,17 @@ import (
 
 // Filter is a convenience method for chaining [it.Filter] on [Iterator]s.
 func (iterator Iterator[V]) Filter(predicate func(V) bool) Iterator[V] {
-	return Iterator[V](it.Filter[V](iter.Seq[V](iterator), predicate))
+	return Iterator[V](it.Filter(iter.Seq[V](iterator), predicate))
 }
 
 // Exclude is a convenience method for chaining [it.Exclude] on [Iterator]s.
 func (iterator Iterator[V]) Exclude(predicate func(V) bool) Iterator[V] {
-	return Iterator[V](it.Exclude[V](iter.Seq[V](iterator), predicate))
+	return Iterator[V](it.Exclude(iter.Seq[V](iterator), predicate))
 }
 
 // Filter is a convenience method for chaining [it.Filter2] on [Iterator2]s.
 func (iterator Iterator2[V, W]) Filter(predicate func(V, W) bool) Iterator2[V, W] {
-	return Iterator2[V, W](it.Filter2[V, W](iter.Seq2[V, W](iterator), predicate))
+	return Iterator2[V, W](it.Filter2(iter.Seq2[V, W](iterator), predicate))
 }
 
 // Exclude is a convenience method for chaining [it.Exclude2] on [Iterator2]s.
