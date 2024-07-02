56011f88794d7171b8cff505dff49d3eb8fbc299
Tom Godkin <tomgodkin@pm.me>
2024-07-02 06:45:30 +0100

    Add FromChannel to itx package

--- it/itx/channel.go
+++ it/itx/channel.go
@@ -6,6 +6,14 @@ import (
 	"github.com/BooleanCat/go-functional/v2/it"
 )
 
+// FromChannel yields values from a channel.
+//
+// In order to avoid a deadlock, the channel must be closed before attempting
+// to called `stop` on a pull-style iterator.
+func FromChannel[V any](channel <-chan V) Iterator[V] {
+	return Iterator[V](it.FromChannel(channel))
+}
+
 // ToChannel is a convenience method for chaining [it.ToChannel] on
 // [Iterator]s.
 func (iterator Iterator[V]) ToChannel() <-chan V {
--- it/itx/channel_test.go
+++ it/itx/channel_test.go
@@ -6,6 +6,24 @@ import (
 	"github.com/BooleanCat/go-functional/v2/it/itx"
 )
 
+func ExampleFromChannel() {
+	items := make(chan int)
+
+	go func() {
+		defer close(items)
+		items <- 1
+		items <- 2
+	}()
+
+	for number := range itx.FromChannel(items) {
+		fmt.Println(number)
+	}
+
+	// Output:
+	// 1
+	// 2
+}
+
 func ExampleIterator_ToChannel() {
 	channel := itx.SlicesValues([]int{1, 2, 3}).ToChannel()
 
