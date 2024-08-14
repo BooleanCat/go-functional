040e869348f430c7d8dc0d709470c2237fe8c9d7
Tom Godkin <tomgodkin@pm.me>
2024-08-14 11:33:56 +0100

    Add DropWhile iterator

--- it/drop_test.go
+++ it/drop_test.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/BooleanCat/go-functional/v2/internal/assert"
 	"github.com/BooleanCat/go-functional/v2/it"
+	"github.com/BooleanCat/go-functional/v2/it/filter"
 )
 
 func ExampleDrop() {
@@ -78,4 +79,88 @@ func TestDrop2YieldFalse(t *testing.T) {
 	numbers(func(v, w int) bool {
 		return false
 	})
+}
+
+func ExampleDropWhile() {
+	for value := range it.DropWhile(slices.Values([]int{1, 2, 3, 4, 1}), filter.LessThan(3)) {
+		fmt.Println(value)
+	}
+
+	// Output:
+	// 3
+	// 4
+	// 1
+}
+
+func TestDropWhileYieldFalse(t *testing.T) {
+	t.Parallel()
+
+	numbers := it.DropWhile(slices.Values([]int{1, 2, 3, 4, 1}), filter.LessThan(3))
+
+	numbers(func(v int) bool {
+		return false
+	})
+}
+
+func TestDropWhileEmpty(t *testing.T) {
+	t.Parallel()
+
+	assert.Empty[int](t, slices.Collect(it.DropWhile(it.Exhausted[int](), filter.Passthrough)))
+}
+
+func TestDropWhileNeverDrop(t *testing.T) {
+	t.Parallel()
+
+	numbers := slices.Collect(it.DropWhile(slices.Values([]int{1, 2, 3}), func(int) bool { return false }))
+	assert.SliceEqual(t, []int{1, 2, 3}, numbers)
+}
+
+func TestDropWhileDropAll(t *testing.T) {
+	t.Parallel()
+
+	numbers := slices.Collect(it.DropWhile(slices.Values([]int{1, 2, 3}), filter.LessThan(4)))
+	assert.Empty[int](t, numbers)
+}
+
+func ExampleDropWhile2() {
+	_, values := it.Collect2(it.DropWhile2(slices.All([]int{1, 2, 3}), func(int, v int) bool {
+		return v < 3
+	}))
+
+	fmt.Println(values)
+	// Output: [3]
+}
+
+func TestDropWhile2YieldFalse(t *testing.T) {
+	t.Parallel()
+
+	numbers := it.DropWhile2(slices.All([]int{1, 2, 3}), func(int, v int) bool {
+		return v < 3
+	})
+
+	numbers(func(int, v int) bool {
+		return false
+	})
+}
+
+func TestDropWhile2Empty(t *testing.T) {
+	t.Parallel()
+
+	_, values := it.Collect2(it.DropWhile2(it.Exhausted2[int, int](), filter.Passthrough2))
+
+	assert.Empty[int](t, values)
+}
+
+func TestDropWhile2NeverDrop(t *testing.T) {
+	t.Parallel()
+
+	_, values := it.Collect2(it.DropWhile2(slices.All([]int{1, 2, 3}), func(int, int) bool { return false }))
+	assert.SliceEqual(t, []int{1, 2, 3}, values)
+}
+
+func TestDropWhile2DropAll(t *testing.T) {
+	t.Parallel()
+
+	_, values := it.Collect2(it.DropWhile2(slices.All([]int{1, 2, 3}), func(int, int) bool { return true }))
+	assert.Empty[int](t, values)
 }
--- it/itx/drop.go
+++ it/itx/drop.go
@@ -11,3 +11,15 @@ func (iterator Iterator[V]) Drop(count uint) Iterator[V] {
 func (iterator Iterator2[V, W]) Drop(count uint) Iterator2[V, W] {
 	return Iterator2[V, W](it.Drop2(iterator, count))
 }
+
+// DropWhile is a convenience method for chaining [it.DropWhile] on
+// [Iterator]s.
+func (iterator Iterator[V]) DropWhile(predicate func(V) bool) Iterator[V] {
+	return Iterator[V](it.DropWhile(iterator, predicate))
+}
+
+// DropWhile is a convenience method for chaining [it.DropWhile2] on
+// [Iterator2]s.
+func (iterator Iterator2[V, W]) DropWhile(predicate func(V, W) bool) Iterator2[V, W] {
+	return Iterator2[V, W](it.DropWhile2(iterator, predicate))
+}
