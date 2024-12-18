b09b5f2aa2d154fd447fc381d7395ea2532b92f1
Tom Godkin <tomgodkin@pm.me>
2024-12-18 11:32:35 +0000

    Provide ref and deref ops for Map

--- it/op/op_test.go
+++ it/op/op_test.go
@@ -17,3 +17,20 @@ func ExampleAdd_string() {
 	fmt.Println(it.Fold(slices.Values([]string{"a", "b", "c"}), op.Add, ""))
 	// Output: abc
 }
+
+func ExampleRef() {
+	refs := slices.Collect(it.Map(slices.Values([]int{5, 6, 7}), op.Ref))
+	fmt.Println(*refs[0], *refs[1], *refs[2])
+	// Output: 5 6 7
+}
+
+func ExampleDeref() {
+	intRef := func(a int) *int {
+		return &a
+	}
+
+	values := slices.Values([]*int{intRef(4), intRef(5), intRef(6)})
+
+	fmt.Println(slices.Collect(it.Map(values, op.Deref)))
+	// Output: [4 5 6]
+}
